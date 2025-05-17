94ad0a061ff8ed750bffecbe60be32765ea4f14d
lalewis1 <lawson@kurrawong.ai>
2025-05-18 00:45:08 +1000

    add features via search bar

--- prez-ui/pages/index.vue
+++ prez-ui/pages/index.vue
@@ -20,8 +20,16 @@
         >
           Browse the Portal &#8594;
         </NuxtLink>
-        <div id="map" class="h-96 mt-8 mx-auto pl-20 pr-20"></div>
       </div>
+      <div class="text-lg mt-4 mb-4">
+        <input
+          id="search"
+          class="px-4 py-2 rounded"
+          type="text"
+          placeholder="search place/language"
+        />
+      </div>
+      <div id="map" class="h-96 mt-8 mx-auto pl-20 pr-20"></div>
     </template>
   </NuxtLayout>
 </template>
@@ -32,6 +40,9 @@ import "ol/ol.css";
 import Map from "ol/Map";
 import View from "ol/View";
 import TileLayer from "ol/layer/Tile";
+import VectorLayer from "ol/layer/Vector.js";
+import VectorSource from "ol/source/Vector.js";
+import WKT from "ol/format/WKT.js";
 import OSM from "ol/source/OSM";
 
 const getTileSourceUrl = () => {
@@ -43,7 +54,56 @@ const getTileSourceUrl = () => {
     : "https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png";
 };
 
+const vectorSource = new VectorSource();
+const vectorLayer = new VectorLayer({ source: vectorSource });
+
+const loadFeatures = async (search_term) => {
+  const query = `
+  PREFIX geo: <http://www.opengis.net/ont/geosparql#>
+  PREFIX schemas: <https://schema.org/>
+  PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+  select ?feature ?name ?wkt
+  where {
+    <https://anufncatalogue.anu.edu.au/refdata-geoms> rdfs:member ?feature .
+    ?feature schemas:name ?name .
+    ?feature geo:hasGeometry/geo:asWKT ?wkt .
+    filter (contains(lcase(?name), "${search_term.toLowerCase()}"))
+  }
+  limit 10
+  `;
+  const url = new URL("http://localhost:8000/sparql");
+  url.searchParams.append("query", query);
+  const headers = new Headers();
+  headers.append("content-type", "application/sparql-query");
+  headers.append("accept", "application/sparql-results+json");
+  try {
+    const response = await fetch(url, { method: "GET", headers: headers });
+    if (!response.ok) {
+      console.log("Error retrieving data");
+    }
+    const data = await response.json();
+    const results = data.results.bindings;
+    vectorSource.clear();
+    for (let i = 0; i < results.length; i++) {
+      const result = results[i];
+      const format = new WKT();
+      const feature = format.readFeature(result.wkt.value, {
+        dataProjection: "EPSG:4326",
+        featureProjection: "EPSG:4326",
+      });
+      feature.setId(result.feature.value);
+      feature.set("name", result.name.value);
+      vectorSource.addFeature(feature);
+    }
+  } catch (error) {
+    console.error("There was a problem with the fetch operation:", error);
+  }
+};
+
 onMounted(() => {
+  document.getElementById("search").addEventListener("change", (event) => {
+    loadFeatures(event.target.value);
+  });
   const map = new Map({
     target: "map",
     layers: [
@@ -52,6 +112,7 @@ onMounted(() => {
           url: getTileSourceUrl(),
         }),
       }),
+      vectorLayer,
     ],
     view: new View({
       center: [133, -25], // Centered on Australia
